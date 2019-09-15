8a8bb0cda78030639e7ea439623e1ef3f5787464
Francisco de Maussion <franciscodemaussion@gmail.com>
2019-09-15 17:20:52 -0300

    Related with V not appearing in QR

--- rocket-qr.py
+++ rocket-qr.py
@@ -1,53 +1,59 @@
-import qrcode, os, sys, importlib, pathlib
 from io import BytesIO
+
+import importlib
+import os
+import pathlib
+import qrcode
+import sys
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4, LETTER
 
 MESSAGE = 'P{:02d} V0{} S0000000'
 TEMPLATE = 'Source/Bases/Rocketbook-{}-Base.pdf'
 GENERATED_PATH = 'Generated'
-OUTPUT_FILENAME = GENERATED_PATH+'/Rocketbook-{}-{}-pages{}.pdf'
+OUTPUT_FILENAME = GENERATED_PATH + '/Rocketbook-{}-{}-pages{}.pdf'
 PATH = 'QR/{}'
 POSITION = {
-    'A4':[508, 62],
-    'Letter':[527, 47],
-    'Mini':[508, 62]
+    'A4': [508, 62],
+    'Letter': [527, 47],
+    'Mini': [215, 12]
 }
+# TODO Read types from database
 TYPES = {
     '0': [
         "DotGrid",
         {
-            'A4':'5',
-            'Letter':'4',
-            'Mini':'3'
+            'A4': '5',
+            'Letter': '4',
+            'Mini': '3'
         }
     ],
     '1': [
         "Graph",
         {
-            'A4':'P',
-            'Letter':'O',
-            'Mini':'S'
+            'A4': 'P',
+            'Letter': 'O',
+            'Mini': 'S'
         }
     ],
     '2': [
         "Lined",
         {
-            'A4':'N',
-            'Letter':'M',
-            'Mini':'V'
+            'A4': 'N',
+            'Letter': 'M',
+            'Mini': 'V'
         }
     ],
     '3': [
         "Music",
         {
-            'A4':'R',
-            'Letter':'Q',
-            'Mini':'X'
+            'A4': 'R',
+            'Letter': 'Q',
+            'Mini': 'X'
         }
     ],
 }
+
 
 def class_for_name(module_name, class_name):
     """
@@ -75,6 +81,7 @@ def class_for_name(module_name, class_name):
         c = (527, 47)
     return c
 
+
 def gen_pdf(quantity=1, frame='A4', type='0', numbered=False):
     """
     Generates a PDF document.
@@ -98,6 +105,10 @@ def gen_pdf(quantity=1, frame='A4', type='0', numbered=False):
         Returns a message with the status
 
     """
+    if frame == "Mini":
+        t = 1
+    else:
+        t = 2
     # Looks for the qr code letter
     frame = frame.lower().capitalize()
     code = TYPES[type][1][frame]
@@ -105,33 +116,34 @@ def gen_pdf(quantity=1, frame='A4', type='0', numbered=False):
     type = TYPES[type][0]
     out_file = OUTPUT_FILENAME.format(frame, type, quantity)
     if os.path.exists(out_file):
-        return "The file %s already exists" % out_file
-    path = PATH.format('/%s/%s' % (frame, type))
+        return f"The file {out_file} already exists"
+    path = PATH.format(f'{frame}{type}')
     frame_class = class_for_name("reportlab.lib.pagesizes", frame.upper())
     pathlib.Path(path).mkdir(parents=True, exist_ok=True)
     output = PdfFileWriter()
-    for num in range(1, int(quantity)+1): # Adjust for the 1 start
+    for num in range(1, int(quantity) + 1):  # Adjust for the 1 start
         # Using ReportLab Canvas to insert image into PDF
         imgTemp = BytesIO()
         imgDoc = canvas.Canvas(imgTemp, pagesize=frame_class)
         # Draw image on Canvas and save PDF in buffer
-        imgDoc.drawImage(qr_generate(num, path, code), int(POSITION[frame][0]), int(POSITION[frame][1]))
+        imgDoc.drawImage(qr_generate(num, path, code, t), int(POSITION[frame][0]), int(POSITION[frame][1]))
         if numbered:
-            imgDoc.drawRightString(int(POSITION[frame][0])-7, int(POSITION[frame][1])+3, str(num))
+            imgDoc.drawRightString(int(POSITION[frame][0]) - 7, int(POSITION[frame][1]) + 3, str(num))
         imgDoc.save()
 
         # Select PageToMerge
         pageToMerge = PdfFileReader(open(TEMPLATE.format(frame, type), "rb")).getPage(0)
-        #pageToMerge = PdfFileReader(open(TEMPLATE, "rb")).getPage(0)
+        # pageToMerge = PdfFileReader(open(TEMPLATE, "rb")).getPage(0)
         pageToMerge.mergePage(PdfFileReader(BytesIO(imgTemp.getvalue())).getPage(0))
         output.addPage(pageToMerge)
     # finally, write "output"
     outputStream = open(out_file, "wb")
     output.write(outputStream)
     outputStream.close()
-    return "The file %s was created" % out_file
+    return f"The file {out_file} was created"
 
-def qr_generate(page_number, path, code):
+
+def qr_generate(page_number, path, code, t=2):
     """
     Generates a qr code image.
 
@@ -152,12 +164,12 @@ def qr_generate(page_number, path, code):
         Returns the path of the jpg file
 
     """
-    file = '%s/%s.jpg' % (path, str(page_number))
+    file = f'{path}/{page_number}.jpg'
     if not os.path.exists(file):
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=2,
+            box_size=t,
             border=1,
         )
         qr.add_data(MESSAGE.format(page_number, code))
@@ -166,9 +178,12 @@ def qr_generate(page_number, path, code):
         img.save(file)
     return file
 
+
 def clean_folders():
     delete_folder(GENERATED_PATH)
     delete_folder(PATH.format(""))
+    print("All clear here")
+
 
 def delete_folder(pth):
     pth = pathlib.Path(pth)
@@ -178,22 +193,26 @@ def delete_folder(pth):
         else:
             sub.unlink()
 
+
 def help_doc(command):
     if command == "create":
-        print("python rocket-qr.py <number of pages> <A4 or letter> <0:DotGrid, 1:Graph, 2:Lined or 3:Music> <number pages (true false)>")
+        print(
+            "python rocket-qr.py <number of pages> <A4 or letter> <0:DotGrid, 1:Graph, 2:Lined or 3:Music> <number "
+            "pages (true false)>")
         print("Generate de PDF doc.")
         print("Default(one page A4 DotGrid false): ")
         print("python rocket-qr.py 1 A4 0 false")
-        print("Is the same as")
-        print("python rocket-qr.py")
+        # print("Is the same as")
+        # print("python rocket-qr.py")
     elif command == "clean":
+        print("Clear all the generated files with:")
         print("python rocket-qr.py -c")
         print("python rocket-qr.py --clean")
-        print("Clear all the generated files.")
     else:
         print("More help with the command, type:")
         print("python rocket-qr.py -h clean")
         print("python rocket-qr.py -h create")
+
 
 if __name__ == '__main__':
     args = tuple(sys.argv[1:])
